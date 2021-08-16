9d9b82c6819e795f836e665b9838b0562e35aa74
Arsh7863 <arshs287@gmail.com.com>
2021-08-16 16:20:28 +0530

    WebRTC start

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     // socket.to(roomId).broadcast.emit("user-connected", userId);
-    socket.to(roomId).emit("user-connected", userId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
   });
 });
 
